e1848b32d28bb9a6d9afd27744e8d037b8696945
mkaimane <m.kai_97@hotmail.com>
2016-04-12 16:55:26 +0200

    1er commit michael

--- client/main.js
+++ client/main.js
@@ -7,6 +7,7 @@ import angular from 'angular';
 import { Meteor } from 'meteor/meteor';
 
 // import { name as Socially } from '../imports/ui/components/socially/socially';
+// hi from Michael
 
 function onReady() {
     angular.bootstrap(document, [
