4b8c8bdf73a744510f67bdd16c2c7436a7efc765
gleguen <germain_lbv@hotmail.fr>
2016-04-12 16:36:26 +0200

    change hello to ubet

---
+++ imports/ui/components/ubet/ubet.js
@@ -0,0 +1,42 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import uiRouter from 'angular-ui-router';
+
+import './ubet.html';
+
+class Ubet {}
+
+const name = 'ubet';
+
+// create a module
+export default angular.module(name, [
+    angularMeteor,
+    uiRouter,
+    'accounts.ui'
+]).component(name, {
+        templateUrl: `imports/ui/components/${name}/${name}.html`,
+        controllerAs: name,
+        controller: Ubet
+    })
+    .config(config)
+    .run(run);
+
+function config($locationProvider, $urlRouterProvider) {
+    'ngInject';
+
+    $locationProvider.html5Mode(true);
+
+    $urlRouterProvider.otherwise('/');
+}
+
+function run($rootScope, $state) {
+    'ngInject';
+
+    $rootScope.$on('$stateChangeError',
+        (event, toState, toParams, fromState, fromParams, error) => {
+            if (error === 'AUTH_REQUIRED') {
+                $state.go('');
+            }
+        }
+    );
+}
