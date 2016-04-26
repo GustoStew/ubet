636abe324a91a83a4b3c26f2c1311ef57a37bee3
atoutprix <fabien.leflanchec@gmail.com>
2016-04-26 16:07:30 +0200

    Connexion

---
+++ imports/ui/components/home/home.js
@@ -0,0 +1,49 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import uiRouter from 'angular-ui-router';
+//import ngMaterial from 'angular-material';
+
+import './home.html';
+
+class Login {
+    constructor($scope, $reactive, $state) {
+        'ngInject';
+
+        $reactive(this).attach($scope);
+
+        this.$state = $state;
+
+    }
+}
+
+const name = 'home';
+
+
+// create a module
+export default angular.module(name, [
+    angularMeteor,
+    uiRouter
+//  ngMaterial
+]).component(name, {
+    templateUrl: `imports/ui/components/${name}/${name}.html`,
+    controllerAs: name
+});
+
+
+function config($stateProvider) {
+    'ngInject';
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            template: '<home></home>',
+            resolve: {
+                currentUser($q) {
+                    if (Meteor.userId() === null) {
+                        return $q.reject('AUTH_REQUIRED');
+                    } else {
+                        return $q.resolve();
+                    }
+                }
+            }
+        });
+}
